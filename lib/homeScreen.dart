25a71bb724232c976e9f6172b4e22984de84a08b
“Riwaj” <shresthariwaj10@gmail.com>
2024-06-27 11:35:55 +0545

    made few changes
    created a todo widget

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:learningflutter/widgets/todo_item.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -31,13 +33,53 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: true,
         // backgroundColor: Colors.redAccent,
       ),
-      body: Column(
-        children: [
-          Container(
-            decoration: BoxDecoration(),
-            child: Text("Hello"),
-          ),
-        ],
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(0),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.black,
+                    size: 20,
+                  ),
+                  prefixIconConstraints: BoxConstraints(
+                    maxHeight: 20,
+                    minWidth: 25,
+                  ),
+                  border: InputBorder.none,
+                  hintText: "Search",
+                  hintStyle: TextStyle(color: Colors.grey[400]),
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 50, bottom: 20),
+                    child: Text(
+                      "All ToDos",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  ToDoItem(),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/todo_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ToDoItem extends StatelessWidget {
+  const ToDoItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
